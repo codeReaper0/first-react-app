710da4c5a01f13c1c34a892dc63c17500f02c40c
codeReaper <boluwatifetella@gmail.com>
2023-03-12 09:49:33 +0100

    Movies components

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,30 +6,48 @@ class Movies extends Component {
     movies: getMovies(),
   };
 
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({movies});
+  };
+
   render() {
+    const {length: count} = this.state.movies;
+
+    if (count === 0)
+      return <p className="p-10">There are no movies in the database</p>;
+
     return (
       <div className="p-10 space-y-6">
         <p>Showing 9 movies in the database</p>
 
-        <table className="divide-y divide-gray-300 space-y-6 w-full text-left">
+        <table className="divide-y divide-gray-300 space-y-8 w-full text-left">
           <thead className="text-lg">
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-            <th></th>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+            </tr>
           </thead>
 
-          <tbody>
-            <td>Terminator</td>
-            <td>Action</td>
-            <td>6</td>
-            <td>2.5</td>
-            <td className="text-center">
-              <button className="text-white bg-red-700 hover:bg-red-800 rounded-md px-6 py-2">
-                Delete
-              </button>
-            </td>
+          <tbody className="">
+            {this.state.movies.map((movie) => (
+              <tr className="" key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td className="text-center">
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="text-white bg-red-700 hover:bg-red-800 rounded-md px-6 py-2"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
